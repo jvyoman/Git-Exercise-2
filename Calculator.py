2bc8ce3f38a716d4046375b2dcb6676db87d8d12
jvyoman <vyomanj@gmail.com>
2020-08-12 12:00:19 +0530

    Initial code added

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+print("Calculator\n")
+val1=input("Enter first number: ")
+val2=input("Enter first number: ")
+
+print("Enter choice:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division\n")
+
+choice=input("Enter choice: ")
+
+if choice==1:[]
+
+elif choice==2:[]
+
+elif choice==3: []
+
+elif choice==4: []
+
+
